5c6e32c7dc8bedca964a10fb4aec21a88d556812
Vivek Panchal <vp443100@gmail.com>
2025-08-08 16:04:19 +0530

    Time: 0 ms (100%), Space: 45.6 MB (78.05%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,11 +3,11 @@ class Solution {
         int start = 0;
         int end = nums.length-1;
         while(start <= end){
-            int mid = start + (end - start)/ 2;
+            int mid = start + (end - start) /2 ;
             if( target < nums[mid]){
-                end = mid -1;
+                end = mid-1;
             }
-            else if(target > nums[mid]){
+            else if( target > nums[mid]){
                 start = mid+1;
             }
             else{
