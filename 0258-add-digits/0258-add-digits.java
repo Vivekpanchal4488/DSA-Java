6440a96982b13a13b20f498d483e88dde36ae4b5
Vivek Panchal <vp443100@gmail.com>
2025-08-19 23:35:45 +0530

    Time: 0 ms (100%), Space: 40.7 MB (85.49%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -3,6 +3,9 @@ class Solution {
         if( num == 0){
             return 0;
         }
-        return ( num%9 == 0) ? 9 : num%9;
+        if( num%9 == 0){
+            return 9;
+        }
+        return num%9;
     }
 }
