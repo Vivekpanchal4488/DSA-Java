b7695298722eaac1a513d7449a90dbaf19ea0e71
Vivek Panchal <vp443100@gmail.com>
2025-08-29 23:58:43 +0530

    Time: 4 ms (100%), Space: 44.7 MB (6.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,9 +6,9 @@ class Solution {
         int rev = 0;
         int num = x;
         while( num != 0){
-            rev = rev* 10 + num % 10;
-            num = num / 10;
+            rev = rev * 10 + num % 10;
+            num = num/10;
         }
-        return ( rev == x);
+        return (rev == x);
     }
 }
