f8df75beb5da54f947268d4f578f71e11b8a9131
Vivek Panchal <vp443100@gmail.com>
2025-10-12 01:03:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.97%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,23 +1,49 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // int start = 0;
+        // int end = nums.length-1;
+
+        // while(start <= end){
+        //     int mid = (start + end ) / 2;
+        //     if( target == nums[mid]){
+        //         return mid;
+        //     }
+        //     if( nums[start] <= nums[mid]){
+        //         if( target <= nums[mid] && target >= nums[start]){
+        //             end = mid-1;
+        //         }
+        //         else{
+        //             start = mid + 1;
+        //         }
+        //     }
+        //     else{
+        //         if(target >= nums[mid] && target <= nums[end]){
+        //             start = mid+1;
+        //         }
+        //         else{
+        //             end = mid-1;
+        //         }
+        //     }
+        // }
+        // return -1;
         int start = 0;
         int end = nums.length-1;
 
         while(start <= end){
-            int mid = (start + end ) / 2;
+            int mid = start +(end-start)/ 2;
             if( target == nums[mid]){
                 return mid;
             }
-            if( nums[start] <= nums[mid]){
-                if( target <= nums[mid] && target >= nums[start]){
+            if(nums[start] <= nums[mid]){
+                if(target <= nums[mid] && target >= nums[start]){
                     end = mid-1;
                 }
                 else{
-                    start = mid + 1;
+                    start = mid+1;
                 }
             }
             else{
-                if(target >= nums[mid] && target <= nums[end]){
+                if(target <= nums[end] && target >= nums[mid]){
                     start = mid+1;
                 }
                 else{
