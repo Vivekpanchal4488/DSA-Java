4a7773e2ff1f0560d7859f95e5fcbe7d0dadd393
Vivek Panchal <vp443100@gmail.com>
2025-10-20 16:36:28 +0530

    Time: 1 ms (99.4%), Space: 57.5 MB (12.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,9 +3,9 @@ class Solution {
         int sum = 0;
         int maxsum = nums[0];
 
-        for(int i =0 ; i< nums.length; i++){
+        for(int i=0; i<nums.length; i++){
             sum = sum + nums[i];
-            if( sum > maxsum){
+            if(sum > maxsum){
                 maxsum = sum;
             }
             if(sum < 0){
