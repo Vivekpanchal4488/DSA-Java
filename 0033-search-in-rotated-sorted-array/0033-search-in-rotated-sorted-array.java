26dc4881489dbea378d8e98abf2f404f604a778e
Vivek Panchal <vp443100@gmail.com>
2026-01-21 16:05:41 +0530

    Time: 0 ms (100%), Space: 44.2 MB (6.23%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,53 +1,26 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        // int start = 0;
-        // int end = nums.length-1;
-
-        // while(start <= end){
-        //     int mid = (start + end ) / 2;
-        //     if( target == nums[mid]){
-        //         return mid;
-        //     }
-        //     if( nums[start] <= nums[mid]){
-        //         if( target <= nums[mid] && target >= nums[start]){
-        //             end = mid-1;
-        //         }
-        //         else{
-        //             start = mid + 1;
-        //         }
-        //     }
-        //     else{
-        //         if(target >= nums[mid] && target <= nums[end]){
-        //             start = mid+1;
-        //         }
-        //         else{
-        //             end = mid-1;
-        //         }
-        //     }
-        // }
-        // return -1;
+    public int search(int[] arr, int target) {
         int start = 0;
-        int end = nums.length-1;
-
+        int end = arr.length-1;
         while(start <= end){
-            int mid = start +(end-start)/ 2;
-            if( target == nums[mid]){
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid]){
                 return mid;
             }
-            if(nums[start] <= nums[mid]){
-                if(target <= nums[mid] && target >= nums[start]){
-                    end = mid-1;
+            if(arr[start] <= arr[mid]){
+                if(target >= arr[start] && target<=arr[mid]){
+                    end = mid -1;
                 }
                 else{
-                    start = mid+1;
+                    start = mid + 1;
                 }
             }
             else{
-                if(target <= nums[end] && target >= nums[mid]){
-                    start = mid+1;
+                if(target <= arr[end] && target >= arr[mid]){
+                    start = mid +1;
                 }
                 else{
-                    end = mid-1;
+                    end = mid -1;
                 }
             }
         }
