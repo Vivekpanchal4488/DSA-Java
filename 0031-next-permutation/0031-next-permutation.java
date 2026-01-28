0ac4d08471de5f0729a5fcc6522261217bdf996e
Vivek Panchal <vp443100@gmail.com>
2026-01-29 00:19:05 +0530

    Time: 0 ms (100%), Space: 44.9 MB (24.35%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,10 +1,10 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums == null || nums.length<=0){
+        if( nums==null || nums.length <= 0){
             return;
         }
         int i = nums.length-2;
-        while(i >= 0 && nums[i] >= nums[i+1]){
+        while( i >=0 && nums[i] >= nums[i+1]){
             i--;
         }
         if(i >= 0){
@@ -12,7 +12,7 @@ class Solution {
             while(j >= 0 && nums[j] <= nums[i]){
                 j--;
             }
-            swap(nums, i ,j);
+            swap(nums, i, j);
         }
         reverse(nums, i+1, nums.length-1);
     }
