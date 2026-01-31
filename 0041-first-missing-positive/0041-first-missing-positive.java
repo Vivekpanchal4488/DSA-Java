98fe877c6c0172ec8044c432158530820d7e4a40
Vivek Panchal <vp443100@gmail.com>
2026-02-01 00:11:30 +0530

    Time: 1 ms (99.52%), Space: 71.2 MB (67.73%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -3,19 +3,19 @@ class Solution {
         int n = nums.length;
         for(int i=0; i<n; i++){
             while( nums[i] > 0 && nums[i] < n && nums[i] != nums[nums[i]-1]){
-                swap(nums, i , nums[i]-1);
+               swap(nums , i ,nums[i]-1); 
             }
         }
-        for(int i=0 ; i<n ; i++){
+        for(int i=0; i<n ; i++){
             if(nums[i] != i+1){
                 return i+1;
             }
         }
-        return n+1;
+            return n+1;
     }
     static void swap(int[] nums, int i, int j){
         int temp = nums[i];
-        nums[i] = nums[j];
+         nums[i] = nums[j];
          nums[j] = temp;
     }
 }
