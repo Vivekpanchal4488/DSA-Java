39483c6ac2c8a719ad3ba3797293bc8dbeed44d3
Vivek Panchal <vp443100@gmail.com>
2026-08-06 11:27:02 +0530

    Time: 2 ms (29.51%), Space: 43.2 MB (16.97%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -2,30 +2,30 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int start = 0;
         int end = 0;
-        for(int i =0; i< nums.length; i++){
-            start = Math.max(start , nums[i]);
+        for(int i=0; i<nums.length; i++){
+            start = Math.max(start, nums[i]);
             end += nums[i];
         }
         while(start < end){
-            int mid = start + ( end - start)/2;
+            int mid = start + (end - start) / 2;
             int sum = 0;
-                int peices = 1;
-            for(int i =0; i< nums.length; i++){
-                if( sum + nums[i] > mid){
-                    sum = nums[i];
-                    peices++;
-                }
-                else{
-                    sum += nums[i];
-                }
-            }
-            if(peices > k){
-                start = mid +1;
+            int parts = 1;
+            for(int i=0; i<nums.length; i++){
+                if(sum + nums[i] > mid ){
+                sum = nums[i];
+                parts++;
             }
             else{
-                end = mid;
+                sum += nums[i];
             }
+            }
+        if(parts > k){
+            start = mid +1;
         }
-    return start;
+        else{
+            end = mid;
+        }
+        }
+        return start;
     }
 }
